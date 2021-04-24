20b2cdd8969867154481981d41aa6fc05e2ecefb
ProjetJava <marine.amr@icloud.com>
2021-04-24 16:33:28 +0200

    Essai

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame fenetre = new JFrame();
-        fenetre.setVisible(true);
+        Window window = new Window();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Arrête le code lorsque l'utilisateur appuie sur la croix
+
+
     }
 }
